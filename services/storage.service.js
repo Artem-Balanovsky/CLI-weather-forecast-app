8c2c56f00d0ae3c70a09b002ed69c370d96961a2
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-08-16 16:24:14 +0300

    Data request from API added

--- services/storage.service.js
+++ services/storage.service.js
@@ -5,6 +5,11 @@ import { promises } from 'fs';
 
 const filePath = join(homedir(), 'weather-data.json');
 
+const TOKEN_DICTIONARY = {
+    token: 'token',
+    city: 'city'
+}
+
 const saveKeyValue = async (key, value) => {
     let data = {};
     if (await isExist(filePath)) {
@@ -20,7 +25,7 @@ const getKeyValue = async (key) => {
     if (await isExist(filePath)) {
         const file = await promises.readFile(filePath);
         const data = JSON.parse(file);
-        return data(key);
+        return data[key];
     }
     return undefined;
 };
@@ -34,4 +39,4 @@ const isExist = async (path) => {
     }
 };
 
-export { saveKeyValue, getKeyValue };
+export { saveKeyValue, getKeyValue, TOKEN_DICTIONARY };
--- weather.js
+++ weather.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Token is not defined')
+        return;
+    }
     try {
-        await saveKeyValue ('token', token);
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
         printSuccess('Token is saved')
     } catch(e) {
         printError(e.message)
     }
-    
 }
 
 const initCLI = () => {
@@ -19,14 +23,12 @@ const initCLI = () => {
         printHelp();
     }
     if (args.s) {
-        // Save City
-
     }
     if (args.t) {
         return saveToken(args.t);
 
     }
-    //Show Weather
+    getWeather('Ko Pha Ngan');
 };
 
 initCLI();
